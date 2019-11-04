5b47cd3142cb0a0f95f96edc4d8cde3119bd25ac
Watcharin Kriengwatana <him10424@gmail.com>
2019-11-04 15:10:53 +0700

    fix(response): fix form data empty body

--- src/response/response.controller.ts
+++ src/response/response.controller.ts
@@ -1,18 +1,21 @@
-import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, UseInterceptors } from '@nestjs/common';
 import { ResponseService } from './response.service';
 import { SubmitResponseDTO } from './response.dto';
 import { Authenticated } from '../auth/auth.decorator';
 import { UserId } from '../user/user.decorator';
+import { AnyFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('response')
 export class ResponseController {
-    constructor(private readonly responseService: ResponseService) {}
+    constructor(private readonly responseService: ResponseService) { }
     @Get(':id')
     getResponse(@Param('id') id: string) {
         return this.responseService.findById(id);
     }
 
+
     @Authenticated()
+    @UseInterceptors(AnyFilesInterceptor())
     @Post()
     submitResponse(
         @Body() response: SubmitResponseDTO,
--- src/response/response.module.ts
+++ src/response/response.module.ts
@@ -4,15 +4,24 @@ import { ResponseController } from './response.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RESPONSE_MODEL, ResponseSchema } from './response.model';
 import { FormModule } from '../form/form.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule } from '../config/config.module';
+import { FileService } from '../file/file.service';
+import { ConfigService } from 'aws-sdk';
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: RESPONSE_MODEL, schema: ResponseSchema },
         ]),
         FormModule,
+        MulterModule.registerAsync({
+            imports: [ConfigModule],
+            useClass: FileService,
+            inject: [ConfigService],
+        }),
     ],
     providers: [ResponseService],
     exports: [ResponseService],
     controllers: [ResponseController],
 })
-export class ResponseModule {}
+export class ResponseModule { }
